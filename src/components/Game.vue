71677ba6a6f09f7b278e0fcf0db6d78921a15381
ArtursAsi <WARlock6665>
2020-05-09 10:09:53 +0300

    first commit

---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="player">
+      <h4 class="player-name">You</h4>
+      <div class="health-bar">
+        <p class="healthRemaining">{{ monsterHealth }}</p>
+        <div class="health-remaining" :style="{}"></div>
+        <div class="health-empty"></div>
+      </div>
+    </div>
+    <div class="player">
+      <h4 class="player-name">MONSTER</h4>
+      <div class="health-bar">
+        <p class="healthRemaining">{{ monsterHealth }}</p>
+        <div class="health-remaining" :style="{}"></div>
+        <div class="health-empty"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {};
+</script>
